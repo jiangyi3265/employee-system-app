a3feea69574dfb629e00206f7df6feab6feba6a1
jiangyi <1774537835@qq.com>
2026-06-07 19:46:44 +0800

    Configure frontend API proxy

--- store/remote.js
+++ store/remote.js
@@ -1,4 +1,6 @@
-const DEFAULT_BASE_URL = 'http://localhost:8080'
+const DEFAULT_BASE_URL = typeof window !== 'undefined' && window.location?.origin
+	? window.location.origin
+	: 'http://localhost:8080'
 
 export const API_BASE_KEY = 'sqms_api_base'
 
